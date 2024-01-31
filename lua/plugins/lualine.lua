132faef1e9666fc1854a3faad4e0721fa27e0548
root <root@JDN-71708.jandenul.com>
2024-01-31 13:16:03 +0100

    Added NeoComposer

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,18 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = 'solarized_dark'
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "solarized_dark",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename", { require("NeoComposer.ui").status_recording } },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
