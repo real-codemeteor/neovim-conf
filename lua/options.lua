3ade522f350d6f4ea781977bd2d1719a3a36f92f
root <root@JDN-71708.jandenul.com>
2024-02-07 10:06:10 +0100

    Enabled hard mode ☢️

--- lua/options.lua
+++ lua/options.lua
@@ -4,4 +4,22 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nu")
 vim.cmd("set termguicolors")
+vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+-- Show an error message if a disabled key is pressed
+local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
+
+-- Disable arrow keys in insert mode with a styled message
+vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap('i', '<BS>', '<C-o>' .. msg, { noremap = true, silent = false })
+
+-- Disable arrow keys in normal mode with a styled message
+vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<Down>", msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<Left>", msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<Right>", msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap('n', '<BS>', msg, { noremap = true, silent = false })
