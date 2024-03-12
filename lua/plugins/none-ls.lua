685522f8dbb9dce678b8ff9e6f290916f9990ea5
🐯 <tim.degraeve@jandenul.com>
2024-03-12 10:45:06 +0100

    Switched to Ruff and added formatting for html

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,31 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.diagnostics.flake8,
+			--	null_ls.builtins.formatting.black,
+			--	null_ls.builtins.formatting.isort,
+				require("none-ls.diagnostics.ruff"),
+                require("none-ls.formatting.ruff"),
 
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
+				--null_ls.builtins.diagnostics.eslint_d,
 
 				null_ls.builtins.diagnostics.hadolint,
 
 				null_ls.builtins.code_actions.refactoring,
+
+				null_ls.builtins.diagnostics.djlint,
+				null_ls.builtins.formatting.djlint,
+				require("none-ls.code_actions.eslint_d"),
+				require("none-ls.diagnostics.eslint_d"),
+
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
