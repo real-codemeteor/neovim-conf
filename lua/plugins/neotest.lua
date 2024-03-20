b8eee7f81dc0efe117e0b60f94abd9589d60d145
CYB De Graeve Tim <adl_cyb_tigr@vm-rhcltigr01.jandenul.com>
2024-03-20 16:06:22 +0100

    Changes made to make work with Tmux

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,46 +1,47 @@
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-neotest/neotest-jest",
-		"nvim-neotest/neotest-python",
-	},
-	config = function()
-		require("neotest").setup({
-			adapters = {
-				require("neotest-python")({
-					dap = { justMyCode = false },
-					runner = "pytest",
-				}),
-				require("neotest-jest")({
-					jestCommand = "npm test --",
-					--                    jestCommand = require("neotest-jest.jest-util").getJestCommand(vim.fn.expand("%:p:h"))
-					-- .. " --watch",
-					jestConfigFile = "jest.config.js",
-					env = { CI = true },
-					cwd = function()
-						return vim.fn.getcwd()
-					end,
-				}),
-			},
-		})
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>ntw",
-			--"<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
-			"<cmd>lua require('neotest').watch.toggle(vim.fn.expand('%'))<cr>",
-			{}
-		)
-		vim.api.nvim_set_keymap("n", "<leader>ntf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", {})
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-jest",
+        "nvim-neotest/neotest-python",
+        "nvim-neotest/nvim-nio",
+    },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-python")({
+                    dap = { justMyCode = false },
+                    runner = "pytest",
+                }),
+                require("neotest-jest")({
+                    jestCommand = "npm test --",
+                    --                    jestCommand = require("neotest-jest.jest-util").getJestCommand(vim.fn.expand("%:p:h"))
+                    -- .. " --watch",
+                    jestConfigFile = "jest.config.js",
+                    env = { CI = true },
+                    cwd = function()
+                        return vim.fn.getcwd()
+                    end,
+                }),
+            },
+        })
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>ntw",
+            --"<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+            "<cmd>lua require('neotest').watch.toggle(vim.fn.expand('%'))<cr>",
+            {}
+        )
+        vim.api.nvim_set_keymap("n", "<leader>ntf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", {})
 
-		vim.api.nvim_set_keymap("n", "<leader>nts", "<cmd>lua require('neotest').summary.toggle()<cr>", {})
+        vim.api.nvim_set_keymap("n", "<leader>nts", "<cmd>lua require('neotest').summary.toggle()<cr>", {})
 
-		vim.api.nvim_set_keymap("n", "<leader>ntr", "<cmd>lua require('neotest').run.run()<cr>", {})
+        vim.api.nvim_set_keymap("n", "<leader>ntr", "<cmd>lua require('neotest').run.run()<cr>", {})
 
-		vim.api.nvim_set_keymap("n", "<leader>nto", "<cmd>lua require('neotest').output_panel.toggle()<cr>", {})
+        vim.api.nvim_set_keymap("n", "<leader>nto", "<cmd>lua require('neotest').output_panel.toggle()<cr>", {})
 
-		vim.api.nvim_set_keymap("n", "<leader>ntd", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", {})
-	end,
+        vim.api.nvim_set_keymap("n", "<leader>ntd", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", {})
+    end,
 }
---
+++ lua/plugins/vim-tpipeline.lua
@@ -0,0 +1,6 @@
+return {
+    "vimpostor/vim-tpipeline",
+    config = function()
+        vim.cmd("set stl=%!tpipeline#stl#line()")
+    end,
+}
